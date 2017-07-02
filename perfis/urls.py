4bd8ba56d4bb5ce0c0421c8f6d9927b9782a0dca
= <danmoura17@gmail.com>
2017-07-02 19:55:42 -0300

    Inserting bootstrap in the application

--- perfis/urls.py
+++ perfis/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from perfis import views
 
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='exibir'),
-    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar')
-]
+    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar'),
+    url(r'^convite/(?P<convite_id>\d+)/aceitar$', views.aceitar, name='aceitar')
+)
